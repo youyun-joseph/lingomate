1f73a2890843a61cc36dd220185ab964fb7f3a6f
youyun-joseph <x_yuuts@hotmail.com>
2025-12-24 15:12:53 +0800

    restart the project

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/lingomate/', // <--- Must match your repository name exactly
-})
